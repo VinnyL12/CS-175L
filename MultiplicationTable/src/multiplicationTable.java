3149df51a84534860023a8e7cdc58aad1f57313d
vinny <vinny@DESKTOP-LNBOQ8A>
2021-04-10 21:40:36 -0400

    Multiplication Table

---
+++ MultiplicationTable/src/multiplicationTable.java
@@ -0,0 +1,17 @@
+
+public class multiplicationTable {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	for(int row = 1; row <= 10; row++)
+	{
+		for(int column=1; column <=10; column++)
+		{
+			System.out.print(String.format("%4d", row * column));
+		}
+		System.out.println();
+		}
+	}
+}
+
